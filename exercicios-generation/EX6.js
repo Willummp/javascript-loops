4f2339355ceed69c6511fdad12397275f47d5ce4
Lucas Ferreira <lsousaah@gmail.com>
2022-09-13 18:19:18 -0300

    Sexto exercício adicionado

--- exercicios-generation/EX6.js
+++ exercicios-generation/EX6.js
@@ -1,5 +1,16 @@
 const prompt = require("prompt-sync")();
 
-let
-let
-let
+let media = 0;
+let n1 = 0;
+let qt = 0;
+
+n1  = parseInt(prompt('Digite um numero do teclado(exceto 0): '));
+do{
+    n1 = parseInt(prompt(`Digite um numero do teclado (exceto 0):  `));
+    if (n1%3==0) {
+        media += media +n1;
+        qt++
+        
+    }
+}while(n1!=0)
+console.log(`A média dos valores foi: ${media/qt}`)
